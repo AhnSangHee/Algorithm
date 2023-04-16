1b230a37d8dd7f7c272d6997e1a5a1f8347a79e3
Sanghee <dev.sanghee@gmail.com>
2023-04-17 00:46:42 +0900

    Initial commit

---
+++ Algorithm/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by 안상희 on 2023/04/17.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    std::ios::sync_with_stdio(false);
+    
+    return 0;
+}
